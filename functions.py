56a494a919a1d5468900336b745872610f4168f7
Tyler Gilroy <tyler.gilroy.23@gmail.com>
2023-04-03 11:57:23 -0600

    "Clean up files"

--- functions.py
+++ functions.py
@@ -1,12 +1,12 @@
 import os
 from datetime import date
 import sqlite3
-connection = sqlite3.connect('registration.db')
+connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
 
 # ------
 
-with open('schema.sql') as outfile:
+with open('Competencies.sql') as outfile:
     queries = outfile.read()
 cursor.executescript(queries)
 
